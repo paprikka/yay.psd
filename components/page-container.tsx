8d628f555187d191e15041d8fad7c1804c363a77
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-03 09:09:25 +0000

    chore: rainbow extract

--- components/page-container.tsx
+++ components/page-container.tsx
@@ -2,26 +2,13 @@ import { FC, useEffect } from "react";
 import styles from "./page-container.module.css";
 import { Nav } from "./nav";
 import { Grid } from "./grid";
+import { useRainbowBg } from "../hooks/use-rainbow-bg";
 
 interface PageContainerProps {
   children: React.ReactNode;
 }
 export const PageContainer: FC<PageContainerProps> = ({ children }) => {
-  useEffect(() => {
-    const cb = () => {
-      const from = 51;
-      const to = 219;
-      const progress =
-        window.scrollY /
-        (document.body.getBoundingClientRect().height - window.innerHeight);
-      document.body.style.backgroundColor = `hsl(${
-        from + (to - from) * progress
-      }deg 100% 50%)`;
-      console.log(progress);
-    };
-    window.addEventListener("scroll", cb, { passive: true });
-    return () => window.removeEventListener("scroll", cb);
-  });
+  useRainbowBg();
   return (
     <div className={styles.container}>
       <Nav />
