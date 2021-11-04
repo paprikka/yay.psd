3a58d98f1067a5262f822aaf151351541cca0ef3
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-04 17:58:08 +0000

    feat: add loading placeholders

--- components/renderers/image.tsx
+++ components/renderers/image.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { PostEntry, PostImage } from "../../data/contentful";
 import Image from "next/image";
 import styles from "./image.module.css";
@@ -7,12 +7,20 @@ interface ImageRendererProps {
   entry: PostEntry;
   image: PostImage;
 }
-export const ImageRenderer: FC<ImageRendererProps> = ({ entry, image }) => (
-  <Image
-    src={"https:" + image.url}
-    alt={entry.title}
-    width={image.width}
-    height={image.height}
-    className={styles.image}
-  />
-);
+
+export const ImageRenderer: FC<ImageRendererProps> = ({ entry, image }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <Image
+      src={"https:" + image.url}
+      alt={entry.title}
+      width={image.width}
+      height={image.height}
+      className={
+        isLoading ? `${styles.image} ${styles.imageLoading}` : styles.image
+      }
+      onLoadingComplete={() => setIsLoading(false)}
+    />
+  );
+};
