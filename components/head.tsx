a41b820903bf919a93febe3bd33dbc229f5e8391
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-04 13:57:46 +0000

    feat: enable per-post social meta

--- components/head.tsx
+++ components/head.tsx
@@ -1,56 +1,27 @@
 import Head from "next/head";
 import { FC } from "react";
 import { SiteConfig, siteConfig, SiteConfigImage } from "../data/site-config";
+import { renderOGMeta } from "./render-og-meta";
 
 interface PageHeadProps {
   title?: string;
   description?: string;
-  socialImage?: SiteConfigImage | null;
+  socialImage?: SiteConfigImage;
+  url?: string;
 }
 
-const defaults: PageHeadProps = {
+const defaults: Required<PageHeadProps> = {
   title: siteConfig.title,
   description: siteConfig.description,
-};
-
-interface OGMetaProps {
-  siteConfig: SiteConfig;
-  socialImage: SiteConfigImage | null;
-}
-
-const renderOGMeta = ({ siteConfig, socialImage }: OGMetaProps) => {
-  const selectedSocialImage: SiteConfigImage =
-    socialImage || siteConfig.socialImage;
-
-  return (
-    <>
-      <meta property="og:description" content={siteConfig.description} />
-      <meta property="og:title" content={siteConfig.title} />
-      <meta property="og:site_name" content={siteConfig.name} />
-      <meta property="og:url" content={siteConfig.url} />
-      <meta property="og:image" content={selectedSocialImage.absoluteUrl} />
-      <meta
-        property="og:image:width"
-        content={selectedSocialImage.width.toString(10)}
-      />
-      <meta
-        property="og:image:height"
-        content={selectedSocialImage.height.toString(10)}
-      />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content={siteConfig.twitterSite} />
-      <meta name="twitter:creator" content={siteConfig.twitterCreator} />
-      <meta name="twitter:title" content={siteConfig.title} />
-      <meta name="twitter:image" content={selectedSocialImage.absoluteUrl} />
-      <meta name="twitter:description" content={siteConfig.description} />
-    </>
-  );
+  url: siteConfig.url,
+  socialImage: siteConfig.socialImage,
 };
 
 export const PageHead: FC<PageHeadProps> = (overrides) => {
-  const { description, title } = { ...defaults, ...overrides };
-  const socialImage = overrides.socialImage || null;
+  const { description, title, url, socialImage } = {
+    ...defaults,
+    ...overrides,
+  };
 
   return (
     <Head>
@@ -58,7 +29,13 @@ export const PageHead: FC<PageHeadProps> = (overrides) => {
       <link rel="icon" type="image/png" href="favicon.png" />
       <link rel="icon" id="favicon" type="image/png" href="favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {renderOGMeta({ siteConfig, socialImage })}
+      {renderOGMeta({
+        description,
+        siteConfig,
+        socialImage,
+        title,
+        url,
+      })}
       <title>{title}</title>
       <meta name="description" content={description} />
     </Head>
