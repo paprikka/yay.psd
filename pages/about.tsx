024010da168bc3c16cde72496d0e2325c6bb7862
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-05 10:09:17 +0000

    feat: add about me copy

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,15 +13,22 @@ const About: NextPage<PageProps> = () => (
       <Image src={headerImg} alt="Rafal Pastuszak" />
       <h1>Hi there</h1>
       <p>
-        My name is Rafal Pastuszak (just like the words <em>pasta</em> and{" "}
-        <em>shack</em>). I&rsquo;m a tinkerer/software engineer currently based
-        in Portugal.
+        My name is Rafal Pastuszak (pronounced like <em>pasta</em> and{" "}
+        <em>shack</em>). I&rsquo;m a visual artist/tinkerer/software engineer
+        currently based in Portugal.
       </p>
       <h2>Contact</h2>
+      <p className={styles.note}>
+        The best way to contact me?{" "}
+        <Link href="https://sonnet.io">
+          <a>Come and say hi</a>
+        </Link>
+        !
+      </p>
       <ul>
         <li>
           <Link href="https://sonnet.io">
-            <a target="_blank">My blog</a>
+            <a target="_blank">My blog (tech, psychology)</a>
           </Link>
         </li>
         <li>
@@ -30,8 +37,18 @@ const About: NextPage<PageProps> = () => (
           </Link>
         </li>
         <li>
+          <Link href="https://rafsters.itch.io/all-hands">
+            <a target="_blank">Games</a>
+          </Link>
+        </li>
+        <li>
           <Link href="https://instagram.com/yay.psd">
             <a target="_blank">Insta</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://instagram.com/rafalpast">
+            <a target="_blank">Photography Insta</a>
           </Link>
         </li>
       </ul>
