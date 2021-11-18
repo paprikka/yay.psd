a44af4dd1a15802c608df064b81b8647fae6f6f8
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-18 12:13:44 +0000

    fix: remove the unused max hue offset from use-raibow

--- hooks/use-rainbow-bg.ts
+++ hooks/use-rainbow-bg.ts
@@ -10,12 +10,11 @@ export const useRainbowBg = () =>
                 contentHeight / viewportHeight
             )
             const from = 51
-            const to = 219
             const progress =
                 window.scrollY / (viewportHeight * viewportsPerRotation)
+            const h = (from + 360 * progress) % 360
 
-            const h = from + (to - from) * progress
-            document.body.style.backgroundColor = `hsl(${h}deg 100% 50%)`
+            document.body.style.backgroundColor = `hsl(${h}deg, 100%, 50%)`
         }
         window.addEventListener('scroll', cb, { passive: true })
         return () => window.removeEventListener('scroll', cb)
