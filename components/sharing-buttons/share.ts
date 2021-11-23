2f5590d39b92b8e51f5d73396f9f927cdf6aa767
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-23 17:42:39 +0000

    fix: add a better test for clipboard support

--- components/sharing-buttons/share.ts
+++ components/sharing-buttons/share.ts
@@ -1,5 +1,5 @@
 export const share = (url: string) => {
-    if (!window.ClipboardItem)
+    if (typeof navigator.clipboard.writeText !== 'function')
         return Promise.reject(new Error('Clipboard access not supported'))
     return navigator.clipboard.writeText(url)
 }
