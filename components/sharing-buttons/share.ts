85051f13f913ea1e4932cabc482f5b796a213d8c
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2021-11-23 17:38:21 +0000

    fix: enable sharing on FF

--- components/sharing-buttons/share.ts
+++ components/sharing-buttons/share.ts
@@ -1,14 +1,5 @@
-const shareLegacy = (url: string) => {
+export const share = (url: string) => {
     if (!window.ClipboardItem)
         return Promise.reject(new Error('Clipboard access not supported'))
-    const text = location.href
-    const type = 'text/plain'
-    const blob = new Blob([text], { type })
-    const data = [new (ClipboardItem as any)({ [type]: blob })]
-
-    return navigator.clipboard.write(data)
-}
-
-export const share = (url: string) => {
-    return shareLegacy(url)
+    return navigator.clipboard.writeText(url)
 }
