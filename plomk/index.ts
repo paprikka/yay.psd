928f3fcf8a689be4e8427cc77d7c24471463e143
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-29 12:45:04 +0100

    wip:

--- plomk/index.ts
+++ plomk/index.ts
@@ -1,25 +1,45 @@
 import { useEffect, useRef } from 'react'
 
-let audio: HTMLAudioElement | null = null
+let audioBuffer: AudioBufferSourceNode | null = null
+async function playAudio(src: string) {
+    let audioContext = new window.AudioContext()
+
+    // If audioBuffer is already defined, stop and release the old source
+    if (audioBuffer) {
+        audioBuffer.stop()
+    }
+
+    // Fetch the audio file
+    let response = await fetch(src)
+    let arrayBuffer = await response.arrayBuffer()
+
+    // Decode the audio file
+    let audioData = await audioContext.decodeAudioData(arrayBuffer)
+
+    // Create a buffer source
+    audioBuffer = audioContext.createBufferSource()
+    audioBuffer.buffer = audioData
+
+    // Connect the source to the context's destination (the speakers)
+    audioBuffer.connect(audioContext.destination)
+
+    // Play the audio
+    audioBuffer.start()
+}
+
 export const usePlomk = () => {
+    let audioRef = useRef<HTMLAudioElement | null>(null)
     console.log('Plomk: Initializing')
     async function plomkNow() {
-        if (!audio) {
-            console.log('Plomk: No audio element')
-            return
-        }
-
-        if (audio.paused) return audio.play()
-
-        audio.currentTime = 0
+        playAudio('/sfx/click_2.mp3')
     }
 
     useEffect(() => {
-        if (!audio) {
+        if (!audioRef.current) {
             console.log('Plomk: Creating audio element')
-            audio = new Audio('/sfx/click_2.mp3')
-            audio.preload = 'auto'
-            audio.oncanplaythrough = () =>
+            audioRef.current = new Audio('/sfx/click_2.mp3')
+            audioRef.current.preload = 'auto'
+            audioRef.current.oncanplaythrough = () =>
                 console.log('Probably can plomk (oncanplaythrough)')
         }
 
